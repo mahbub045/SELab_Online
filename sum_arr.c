779a90f01a74287a46d07b47275d9926f5435cdf
mahbub099 <mahbubrmn99@gmail.com>
2022-01-09 15:27:06 +0600

    update sum_arr file

--- sum_arr.c
+++ sum_arr.c
@@ -1,19 +1,26 @@
 #include<stdio.h>
-int sum(int arr[], int n) {
- for (int i=0;i<n;i++){
- arr[i] += 5;
- }
- Return arr[n-1];
-}
-int main() {
- int arr[100];
- int i,j,k,n;
- printf("Enter size of array ");
- // take array size input
- for(i=0;i<n;i++)
- {
- // take array elements input
- }
- printf("sum of the elements of this array is %d", sum(arr, n));
- return 0;
-}
+int sum(int arr[], int n)
+{
+    for (int i=0; i<n; i++)
+    {
+        int s = 0;
+        int i;
+        for (i=0; i<n; i++)
+        {
+            s += arr[i];
+        }
+        return s;
+    }
+    int main()
+    {
+        int arr[100];
+        int i,j,k,n;
+        printf("Enter size of array ");
+// take array size input
+        for(i=0; i<n; i++)
+        {
+// take array elements input
+        }
+        printf("sum of the elements of this array is %d", sum(arr, n));
+        return 0;
+    }
